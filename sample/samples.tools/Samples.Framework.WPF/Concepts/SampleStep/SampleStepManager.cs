bf9587c6929a64e2fbe3703b50b80d78d57781ee
Chrisfactory <chris.ohl67@gmail.com>
2021-12-16 09:18:38 +0100

    add CurrentStep

--- sample/samples.tools/Samples.Framework.WPF/Concepts/SampleStep/SampleStepManager.cs
+++ sample/samples.tools/Samples.Framework.WPF/Concepts/SampleStep/SampleStepManager.cs
@@ -9,11 +9,26 @@ namespace Samples.Framework.WPF.Concepts.SampleStep
 {
     public class SampleStepManager : ViewModelBase
     {
+        private ISampleStep? _CurrentStep;
         public SampleStepManager(IEnumerable<ISampleStep> steps)
         {
             StepsTems = new ObservableCollection<ISampleStep>(steps);
+            CurrentStep= StepsTems.FirstOrDefault();
         }
 
-        public ObservableCollection<ISampleStep> StepsTems { get;  }
+        public ObservableCollection<ISampleStep> StepsTems { get; }
+
+        public ISampleStep? CurrentStep
+        {
+            get { return _CurrentStep; }
+            set
+            {
+                if (this._CurrentStep != value)
+                {
+                    _CurrentStep = value;
+                    base.Notify();
+                }
+            }
+        }
     }
 }
